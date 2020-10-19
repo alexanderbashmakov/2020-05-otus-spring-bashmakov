65eb1d5e1aa09c3d97c8351d6339c74f3a0c8287
abashmakov <abashmakov@diasoft.ru>
2020-10-19 14:35:00 +0300

    initial commit

---
+++ Task_11_Spring_WebFlux/src/main/java/ru/otus/library/service/CommentServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.otus.library.service;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.library.domain.Comment;
+import ru.otus.library.dto.CommentDto;
+import ru.otus.library.repository.CommentRepository;
+
+import java.text.SimpleDateFormat;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+    private final CommentRepository commentRepository;
+    private final IOService ioService;
+    private final MessageBundleService bundleService;
+
+    @Transactional
+    @Override
+    public void create(@NonNull String bookId, Comment comment) {
+        commentRepository.create(bookId, comment);
+    }
+
+    @Transactional
+    @Override
+    public void update(@NonNull String id, Comment comment) {
+        commentRepository.update(id, comment);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(String id) {
+        commentRepository.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public void deleteAll() {
+        commentRepository.deleteAll();
+    }
+}
